9dc9b8a0a9b240f74de04ebeddd0c1e7cbf64395
alikhandkk81 <alikhandkk81@gmail.com>
2026-02-05 17:47:38 +0100

    first commit

---
+++ my_portfolio/portfolio/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'home.html',{'projects':projects})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
